f1f93cef944cc9d0e63516cd0614ed911e8e06cc
jorge <jorge@incsteps.com>
2026-09-24 18:20:20 +0200

    fix package

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
                 delete pkg.devDependencies;
                 delete pkg.scripts;
                 pkg.main = 'index.html';
+                if (!pkg.logseq) {
+                    pkg.logseq = {};
+                }
+                pkg.logseq.icon = './icon.png';
                 fs.writeFileSync(
                     path.resolve(__dirname, 'dist/package.json'),
                     JSON.stringify(pkg, null, 2)
